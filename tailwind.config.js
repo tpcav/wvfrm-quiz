082047737a1dbe2dcb99e2f8a15af43e36d65975
c <thomascavalcante77@gmail.com>
2023-01-05 20:43:54 -0500

    tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./dashboard/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
