c32356791861926ce165190ecc7b1533bb74457a
c <thomascavalcante77@gmail.com>
2023-01-06 01:36:10 -0500

    quiz UI change

--- dashboard/QuizList.js
+++ dashboard/QuizList.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { AiFillPlayCircle} from "react-icons/ai"
 
 const people = [
   {
@@ -13,7 +14,7 @@ const people = [
     name: 'November',
     quizUrl: './dec2022',
     email: 'leslie.alexander@example.com',
-    role: '20 questions',
+    role: 'Coming soon...',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -21,7 +22,7 @@ const people = [
     name: 'October',
     quizUrl: './dec2022',
     email: 'leslie.alexander@example.com',
-    role: '25 questions',
+    role: 'Coming soon...',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -29,7 +30,7 @@ const people = [
     name: 'September',
     quizUrl: './dec2022',
     email: 'leslie.alexander@example.com',
-    role: '25 questions',
+    role: 'Coming soon...',
     imageUrl:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   },
@@ -48,7 +49,7 @@ export default function QuizList() {
             key={person.email}
             className="relative flex items-center space-x-3 rounded-lg border border-zinc-600 bg-zinc-700 px-6 py-2 shadow-sm focus-within:ring-2 focus-within:ring-red-500 focus-within:ring-offset-2 hover:border-gray-400"
           >
-            <div className="min-w-0 flex-1">
+            <div className="min-w-0 flex-1 floot-root">
               <Link href={person.quizUrl} className="focus:outline-none">
                 <span className="absolute inset-0" aria-hidden="true" />
                 <p className="text-sm font-medium text-zinc-200">{person.name}</p>
