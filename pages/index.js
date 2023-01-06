9a80ea85c95b74d563f5a3b63177ffe31fa63e8f
c <thomascavalcante77@gmail.com>
2023-01-05 21:27:05 -0500

    tests

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <title>WVFRM Quiz</title>
       </Head>
       <div className="p-6 m-4 rounded-lg">
-        <div className="px-12 text-center">
+        <div className="text-left">
           <h1 className="text-3xl text-white font-bold">WVFRM Podcast Trivia</h1>
           <p className="mt-4 text-gray-500">Wacky quizzies complied from all of the&nbsp;
             <span><Link href="https://www.youtube.com/c/Waveform" className="underline text-red-400">Waveform Podcast </Link></span>
